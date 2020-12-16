22ac1b085a8ceee1d852e1a28842f7bbd3511e22
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-12-15 18:22:18 -0800

    replaced app.test.js, completed Log.js, added controller folder and recipes.js and moved routes to recipes.js

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,16 +1,16 @@
 DROP TABLE IF EXISTS recipes;
+-- DROP TABLE IF EXISTS logs;
 
 CREATE TABLE recipes (
   id BIGINT GENERATED ALWAYS AS IDENTITY,
   name TEXT NOT NULL,
-  ingredients TEXT [],
   directions TEXT[]
 );
 
-CREATE TABLE logs (
-  id BIGINT GENERATED ALWAYS AS IDENTITY,
-  dateOfEvent DATE NOT NULL,
-  notes TEXT, 
-  rating decimal(2,1) CONSTRAINT rating CHECK (rating >= 0 AND rating <= 5),
-  recipeId BIGINT NOT NULL REFERENCES recipes(id)
-);
+-- CREATE TABLE logs (
+--   id BIGINT GENERATED ALWAYS AS IDENTITY,
+--   date_of_event DATE NOT NULL,
+--   notes TEXT, 
+--   rating decimal(2,1) CONSTRAINT rating CHECK (rating >= 0 AND rating <= 5),
+--   recipe_id BIGINT NOT NULL REFERENCES recipes(id)
+-- );
